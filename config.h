45857f39c1f3db8f33be614c12651844b5413e6b
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-02-21 19:57:49 +0530

    Format everything using clang-format

--- config.h
+++ config.h
@@ -4,64 +4,102 @@
 
 /* appearance */
 static const char *audioctl[] = {"/usr/bin/pavucontrol", NULL};
-static const char *screenshot[] = {"/usr/bin/scrot", "/home/ark/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL};
-static const char *brightup[] = {"/usr/bin/xbacklight", "-inc", "5", NULL};
-static const char *brightdown[] = {"/usr/bin/xbacklight", "-dec", "5", NULL};
-static const char *upvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL};
-static const char *downvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL};
-static const char *mutevol[] = {"/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL};
+static const char *slockctl[] = {"/usr/local/bin/slock", NULL};
+static const char *screenshot[] = {"/usr/local/bin/scrnctl", "/home/ark/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL};
+// static const char *brightup[] = {"/usr/bin/xbacklight", "-inc", "5", NULL};
+// static const char *brightdown[] = {"/usr/bin/xbacklight", "-dec", "5", NULL};
+static const char *brightup[] = {"/usr/local/bin/brictl", "-u", NULL};
+static const char *brightdown[] = {"/usr/local/bin/brictl", "-d", NULL};
+// static const char *upvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL};
+// static const char *downvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL};
+// static const char *mutevol[] = {"/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL};
+static const char *upvol[] = {"/usr/local/bin/volctl", "-u", NULL};
+static const char *downvol[] = {"/usr/local/bin/volctl", "-d", NULL};
+static const char *mutevol[] = {"/usr/local/bin/volctl", "-m", NULL};
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"JetBrains Mono:size=9"};
-static const char dmenufont[] = "JetBrains Mono:size=9";
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=9"};
+static const char dmenufont[] = {"JetBrainsMono Nerd Font:size=9"};
 
-static const char norm_bg[] = "#2f383e";
-static const char norm_border[] = "#868d80";
-static const char norm_fg[] = "#d8caac";
-static const char sel_fg[] = "#2f383e";
-static const char sel_bg[] = "#a7c080";
-static const char sel_border[] = "#a7c080";
+// onedark colorscheme
+// static const char norm_fg[] = "#abb2bf";
+// static const char norm_bg[] = "#282c34";
+// static const char norm_border[] = "#abb2bf";
+//
+// static const char sel_fg[] = "#282c34";
+// static const char sel_bg[] = "#98c379";
+// static const char sel_border[] = "#e5c07b";
+//
+// static const char *colors[][3] = {
+//    /*               fg           bg         border                         */
+//    [SchemeNorm] = {norm_fg, norm_bg, norm_border}, // unfocused wins
+//    [SchemeSel] = {sel_fg, sel_bg, sel_border},     // the focused win
+//};
+
+static const char background[] = "#1e1e2e";
+static const char bg_alt[] = "#181825";
+static const char foreground[] = "#cdd6f4";
+static const char cyan[] = "#89dceb";
+static const char green[] = "#a6e3a1";
+static const char orange[] = "#fab387";
+static const char pink[] = "#f5c2e7";
+static const char purple[] = "#cba6f7";
+static const char red[] = "#f38ba8";
+static const char yellow[] = "#f9e2af";
+static const char blue[] = "#89b4fa";
 
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {norm_fg, norm_bg, norm_border},
-    [SchemeSel] = {sel_fg, sel_bg, sel_border},
+    [SchemeNorm] = {foreground, background, background},
+    [SchemeSel] = {background, green, orange},
 };
 
-// gruvbox soft theme
-// static const char norm_fg[] = "#fbf1c7";
-// static const char norm_bg[] = "#32302f";
-// static const char norm_border[] = "#665c54";
+// everforest colorscheme
+// static const char norm_bg[] = "#2f383e";
+// static const char norm_border[] = "#2f383e";
+// static const char norm_fg[] = "#d8caac";
+// static const char sel_fg[] = "#2f383e";
+// static const char sel_bg[] = "#a7c080";
+// static const char sel_border[] = "#a7c080";
 //
-// static const char sel_fg[] = "#32302f";
-// static const char sel_bg[] = "#b8bb26";
-// static const char sel_border[] = "#fbf1c7";
-//
-// static const char *colors[][3]      = {
-//    /*               fg           bg         border                         */
-//    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
-//    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+// static const char *colors[][3] = {
+//    /*               fg         bg         border   */
+//    [SchemeNorm] = {norm_fg, norm_bg, norm_border},
+//    [SchemeSel] = {sel_fg, sel_bg, sel_border},
 //};
 
-// dracula theme
-// static const char norm_fg[]     = "#ff6e67";
-// static const char norm_bg[]     = "#282a36";
+// gruvbox colorscheme
+// static const char norm_fg[] = "#bdae93";
+// static const char norm_bg[] = "#282828";
+// static const char norm_border[] = "#504945";
+// static const char sel_bg[] = "#504945";
+// static const char sel_fg[] = "#ebdbb2";
+// static const char sel_border[] = "#ebdbb2";
+// static const char *colors[][3] = {
+//    /*               fg         bg         border   */
+//    [SchemeNorm] = {norm_fg, norm_bg, norm_border},
+//    [SchemeSel] = {sel_fg, sel_bg, sel_border},
+//};
+
+// dracula colorscheme
+// static const char norm_fg[] = "#ff6e67";
+// static const char norm_bg[] = "#282a36";
 // static const char norm_border[] = "#ff6e67";
 //
-// static const char sel_fg[]      = "#bfbfbf";
-// static const char sel_bg[]      = "#282a36";
-// static const char sel_border[]  = "#dfdfdd";
+// static const char sel_fg[] = "#bfbfbf";
+// static const char sel_bg[] = "#282a36";
+// static const char sel_border[] = "#dfdfdd";
 //
-// static const char *colors[][3]      = {
+// static const char *colors[][3] = {
 //    /*               fg           bg         border                         */
-//    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
-//    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+//    [SchemeNorm] = {norm_fg, norm_bg, norm_border}, // unfocused wins
+//    [SchemeSel] = {sel_fg, sel_bg, sel_border},     // the focused win
 //};
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {"", "", "", "", "", "", "", "", ""};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -108,8 +146,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg,
-    "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL};
+    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", background,
+    "-nf", foreground, "-sb", background, "-sf", foreground, NULL};
 static const char *termcmd[] = {"alacritty", NULL};
 
 static Key keys[] = {
@@ -121,6 +159,7 @@ static Key keys[] = {
     {0, XF86XK_AudioMute, spawn, {.v = mutevol}},
     {0, XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
     {MODKEY | ShiftMask, XK_a, spawn, {.v = audioctl}},
+    {MODKEY | ShiftMask, XK_l, spawn, {.v = slockctl}},
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, togglebar, {0}},

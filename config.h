c0c744a5f21481424d2857476018200c020123ed
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-03-03 02:54:30 +0530

    Added the 'actualfullscreen' patch
    Added some colorschemes

--- config.h
+++ config.h
@@ -5,17 +5,17 @@
 /* appearance */
 static const char *audioctl[] = {"/usr/bin/pavucontrol", NULL};
 static const char *slockctl[] = {"/usr/local/bin/slock", NULL};
-static const char *screenshot[] = {"/usr/local/bin/scrnctl", "/home/ark/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png", NULL};
+static const char *screenshot[] = {"/usr/local/bin/screencap", NULL};
 // static const char *brightup[] = {"/usr/bin/xbacklight", "-inc", "5", NULL};
 // static const char *brightdown[] = {"/usr/bin/xbacklight", "-dec", "5", NULL};
-static const char *brightup[] = {"/usr/local/bin/brictl", "-u", NULL};
-static const char *brightdown[] = {"/usr/local/bin/brictl", "-d", NULL};
+static const char *brightup[] = {"/usr/local/bin/backlightctl", "-u", NULL};
+static const char *brightdown[] = {"/usr/local/bin/backlightctl", "-d", NULL};
 // static const char *upvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL};
 // static const char *downvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL};
 // static const char *mutevol[] = {"/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL};
-static const char *upvol[] = {"/usr/local/bin/volctl", "-u", NULL};
-static const char *downvol[] = {"/usr/local/bin/volctl", "-d", NULL};
-static const char *mutevol[] = {"/usr/local/bin/volctl", "-m", NULL};
+static const char *upvol[] = {"/usr/local/bin/volumectl", "-u", NULL};
+static const char *downvol[] = {"/usr/local/bin/volumectl", "-d", NULL};
+static const char *mutevol[] = {"/usr/local/bin/volumectl", "-m", NULL};
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
@@ -23,83 +23,19 @@ static const int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {"JetBrainsMono Nerd Font:size=9"};
 static const char dmenufont[] = {"JetBrainsMono Nerd Font:size=9"};
 
-// onedark colorscheme
-// static const char norm_fg[] = "#abb2bf";
-// static const char norm_bg[] = "#282c34";
-// static const char norm_border[] = "#abb2bf";
-//
-// static const char sel_fg[] = "#282c34";
-// static const char sel_bg[] = "#98c379";
-// static const char sel_border[] = "#e5c07b";
-//
-// static const char *colors[][3] = {
-//    /*               fg           bg         border                         */
-//    [SchemeNorm] = {norm_fg, norm_bg, norm_border}, // unfocused wins
-//    [SchemeSel] = {sel_fg, sel_bg, sel_border},     // the focused win
-//};
-
-static const char background[] = "#1e1e2e";
-static const char bg_alt[] = "#181825";
-static const char foreground[] = "#cdd6f4";
-static const char cyan[] = "#89dceb";
-static const char green[] = "#a6e3a1";
-static const char orange[] = "#fab387";
-static const char pink[] = "#f5c2e7";
-static const char purple[] = "#cba6f7";
-static const char red[] = "#f38ba8";
-static const char yellow[] = "#f9e2af";
-static const char blue[] = "#89b4fa";
-
+static const char col_gray1[] = "#282828";
+static const char col_gray2[] = "#282828";
+static const char col_gray3[] = "#ebdbb2";
+static const char col_gray4[] = "#282828";
+static const char col_cyan[] = "#ebdbb2";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {foreground, background, background},
-    [SchemeSel] = {background, green, orange},
+    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
+    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
 };
 
-// everforest colorscheme
-// static const char norm_bg[] = "#2f383e";
-// static const char norm_border[] = "#2f383e";
-// static const char norm_fg[] = "#d8caac";
-// static const char sel_fg[] = "#2f383e";
-// static const char sel_bg[] = "#a7c080";
-// static const char sel_border[] = "#a7c080";
-//
-// static const char *colors[][3] = {
-//    /*               fg         bg         border   */
-//    [SchemeNorm] = {norm_fg, norm_bg, norm_border},
-//    [SchemeSel] = {sel_fg, sel_bg, sel_border},
-//};
-
-// gruvbox colorscheme
-// static const char norm_fg[] = "#bdae93";
-// static const char norm_bg[] = "#282828";
-// static const char norm_border[] = "#504945";
-// static const char sel_bg[] = "#504945";
-// static const char sel_fg[] = "#ebdbb2";
-// static const char sel_border[] = "#ebdbb2";
-// static const char *colors[][3] = {
-//    /*               fg         bg         border   */
-//    [SchemeNorm] = {norm_fg, norm_bg, norm_border},
-//    [SchemeSel] = {sel_fg, sel_bg, sel_border},
-//};
-
-// dracula colorscheme
-// static const char norm_fg[] = "#ff6e67";
-// static const char norm_bg[] = "#282a36";
-// static const char norm_border[] = "#ff6e67";
-//
-// static const char sel_fg[] = "#bfbfbf";
-// static const char sel_bg[] = "#282a36";
-// static const char sel_border[] = "#dfdfdd";
-//
-// static const char *colors[][3] = {
-//    /*               fg           bg         border                         */
-//    [SchemeNorm] = {norm_fg, norm_bg, norm_border}, // unfocused wins
-//    [SchemeSel] = {sel_fg, sel_bg, sel_border},     // the focused win
-//};
-
 /* tagging */
-static const char *tags[] = {"", "", "", "", "", "", "", "", ""};
+static const char *tags[] = {"term", "edit", "www", "file", "comm", "music", "video", "misc", "misc"};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -107,10 +43,13 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class, instance, title, tags mask, iscentered, isfloating, monitor */
-    {"Pavucontrol", NULL, NULL, 0, 1, 1, -1},
     {"Nitrogen", NULL, NULL, 0, 1, 1, -1},
-    {"Gimp", NULL, NULL, 0, 0, 0, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, 0, -1},
+    {"Alacritty", NULL, NULL, 0, 0, 0, -1},
+    {"Nemo", NULL, NULL, 1 << 3, 0, 0, -1},
+    {"Code", NULL, NULL, 1 << 1, 0, 0, -1},
+    {"Pavucontrol", NULL, NULL, 0, 1, 1, -1},
+    {"firefox", NULL, NULL, 1 << 2, 0, 0, -1},
+    {"Spotify", NULL, NULL, 1 << 5, 0, 0, -1},
 };
 
 /* layout(s) */
@@ -146,13 +85,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", background,
-    "-nf", foreground, "-sb", background, "-sf", foreground, NULL};
+    "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1,
+    "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
 static const char *termcmd[] = {"alacritty", NULL};
 
 static Key keys[] = {
     /* modifie              rkey                        function argument */
     {0, XK_Print, spawn, {.v = screenshot}},
+    {0, XF86XK_MonBrightnessUp, spawn, {.v = brightup}},
     {0, XF86XK_MonBrightnessUp, spawn, {.v = brightup}},
     {0, XF86XK_MonBrightnessDown, spawn, {.v = brightdown}},
     {0, XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
@@ -160,6 +100,7 @@ static Key keys[] = {
     {0, XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
     {MODKEY | ShiftMask, XK_a, spawn, {.v = audioctl}},
     {MODKEY | ShiftMask, XK_l, spawn, {.v = slockctl}},
+    {MODKEY | ShiftMask, XK_f, togglefullscr, {0}},
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, togglebar, {0}},
--- dwm.c
+++ dwm.c
@@ -232,6 +232,7 @@ static void tagmon(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
@@ -329,6 +330,12 @@ void applyrules(Client *c) {
     if (ch.res_name)
         XFree(ch.res_name);
     c->tags = c->tags & TAGMASK ? c->tags & TAGMASK : c->mon->tagset[c->mon->seltags];
+}
+
+/* actual fullscreen patch */
+void togglefullscr(const Arg *arg) {
+    if (selmon->sel)
+        setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 int applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact) {
